e72702f515f6a72fc038bd92e779b2ff281ab8f3
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-08-04 01:00:37 -0300

    feat: Add form filling functionality for site layout 'layout1'

--- content/content.js
+++ content/content.js
@@ -5,24 +5,28 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 });
 
 function fillForm(data) {
-  const accountField = document.querySelector(".ant-select-search__field");
-  const passwordFields = document.querySelectorAll("input[type='password']");
-  const nameField = document.querySelector(
-    "input[placeholder='Preencha o nome verdadeiro e torne -o conveniente para a retirada posterior!']"
-  );
-  const agreementCheckbox = document.querySelector(".ant-checkbox-input");
-  const registerButton = document.querySelector("button:contains('Registro')");
+  if (data.siteLayout === 'layout1') {
+    const accountField = document.querySelector(".ant-select-search__field");
+    const passwordFields = document.querySelectorAll("input[type='password']");
+    const nameField = document.querySelector(
+      "input[placeholder='Preencha o nome verdadeiro e torne -o conveniente para a retirada posterior!']"
+    );
+    const agreementCheckbox = document.querySelector(".ant-checkbox-input");
+    const registerButton = document.querySelector("button:contains('Registro')");
 
-  if (accountField) accountField.value = data.username;
+    if (accountField) accountField.value = data.username;
+    
+    passwordFields.forEach((field) => (field.value = data.password));
+
+    if (nameField) nameField.value = data.name;
+
+    if (agreementCheckbox && !agreementCheckbox.checked)
+      agreementCheckbox.click();
+
+    if (registerButton) registerButton.click();
+  }
   
-  passwordFields.forEach((field) => (field.value = data.password));
-
-  if (nameField) nameField.value = data.name;
-
-  if (agreementCheckbox && !agreementCheckbox.checked)
-    agreementCheckbox.click();
-
-  if (registerButton) registerButton.click();
+  // Adicione mais layouts conforme necessário
 }
 
 export { fillForm };
