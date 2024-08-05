17ced6af03cf91541557ad979f089896d7f33347
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-08-05 00:27:37 -0300

    refactor: Update StorageService to use static method for saving user

--- src/background/services/storage-service.ts
+++ src/background/services/storage-service.ts
@@ -1,22 +1,8 @@
 import { User } from "../models/user";
 
 export class StorageService {
-  private users: User[] = [];
-
-  public addUser(user: User) {
-    this.users.push(user);
-    chrome.storage.local.set({ users: this.users });
-  }
-
-  public getUsers(): Promise<User[]> {
-    return new Promise((resolve, reject) => {
-      chrome.storage.local.get(["users"], (result) => {
-        if (result.users) {
-          resolve(result.users);
-        } else {
-          reject("No users found");
-        }
-      });
-    });
+  static saveUser(user: User): void {
+    // Implementar lógica para salvar usuário em armazenamento local
+    console.log("Usuário salvo:", user);
   }
 }
