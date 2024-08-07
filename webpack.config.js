66fb101d6dba9874cb74376f479711f4fed235a5
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-08-07 17:27:03 -0300

    refactor: Add Chrome types to tsconfig.json and copy necessary files in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -22,4 +23,13 @@ module.exports = {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "src/manifest.json", to: "manifest.json" },
+        { from: "src/popup/popup.html", to: "popup.html" },
+        { from: "src/popup/popup.css", to: "popup.css" },
+      ],
+    }),
+  ],
 };
