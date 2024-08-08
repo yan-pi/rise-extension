518571e944c5094251f63680b7c49e25a390a6e3
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-08-08 17:57:49 -0300

    feat: Add new generic site layout with form selectors and data fields

--- src/config/site-layouts.ts
+++ src/config/site-layouts.ts
@@ -6,6 +6,43 @@ export interface SiteLayout {
 
 export const siteLayouts: SiteLayout[] = [
   {
+    name: "Generic",
+    selectors: {
+      form: "form.ant-form",
+      username:
+        "input[name='username'], input[placeholder*='usuário'], input[placeholder*='username'], input.ant-select-search__field, input[placeholder='Nome de Usuário'], input[placeholder='Nome de Usuário']",
+      password:
+        "input[type='password'], input[placeholder*='senha'], input[placeholder*='password'], input[type='password'],input[type='password']",
+      passwordConfirm:
+        "input[placeholder*='confirme'], input[placeholder*='confirm password'], input[placeholder='Por favor, confirme sua senha novamente'], input[placeholder='Confirme a senha novamente, o mesmo que a senha!']",
+      realName:
+        "input[placeholder*='nome'], input[placeholder*='real name'] input[placeholder='Preencha o nome verdadeiro e torne -o conveniente para a retirada posterior!'], input[placeholder='Preencha o nome verdadeiro e torne -o conveniente para a retirada posterior!']",
+      email: "input[type='email'], input[placeholder*='email']",
+      cpf: "input[placeholder*='cpf'], input[placeholder*='ssn']",
+      phoneNumber:
+        "input[type='tel'], input[placeholder*='telefone'], input[placeholder*='phone'], input[placeholder='Digite o Número do Celular']",
+      firstName:
+        "input[placeholder*='primeiro nome'], input[placeholder*='first name']",
+      lastName:
+        "input[placeholder*='sobrenome'], input[placeholder*='last name']",
+      submit:
+        "button[type='submit'], button[placeholder*='enviar'], button[placeholder='Registro'], button[placeholder*='submit'], button.ant-btn-primary, button.ant-btn, button.ant-btn.ant-btn-primary.ant-btn-block.GaL3XJonIwzK4ZeJyCyq",
+      agreeCheckbox:
+        "input[type='checkbox'], input[placeholder*='concordo'], input[placeholder*='agree'], input.ant-checkbox-input",
+    },
+    dataFields: [
+      "username",
+      "password",
+      "passwordConfirm",
+      "realName",
+      "email",
+      "cpf",
+      "phoneNumber",
+      "firstName",
+      "lastName",
+    ],
+  },
+  {
     name: "BraApp",
     selectors: {
       form: "form.ant-form",
