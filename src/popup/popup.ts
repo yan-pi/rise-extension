9e55fdbf534065a62b50f903609af152553f9863
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-08-10 13:11:24 -0300

    style: Update popup.css and popup.html to handle custom and random password

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -25,6 +25,32 @@ document.addEventListener("DOMContentLoaded", () => {
     layoutSelector.appendChild(option);
   });
 
+  // Set initial state of checkboxes to true
+  useRandomPassword.checked = true;
+  enableAdBlocker.checked = true;
+
+  // Disable predefined password input if useRandomPassword is checked
+  predefinedPassword.disabled = useRandomPassword.checked;
+
+  // Restore checkbox states from localStorage
+  useRandomPassword.checked =
+    localStorage.getItem("useRandomPassword") === "true" || true;
+  enableAdBlocker.checked =
+    localStorage.getItem("enableAdBlocker") === "true" || true;
+
+  // Save checkbox states to localStorage when changed
+  useRandomPassword.addEventListener("change", () => {
+    localStorage.setItem(
+      "useRandomPassword",
+      useRandomPassword.checked.toString()
+    );
+    predefinedPassword.disabled = useRandomPassword.checked;
+  });
+
+  enableAdBlocker.addEventListener("change", () => {
+    localStorage.setItem("enableAdBlocker", enableAdBlocker.checked.toString());
+  });
+
   generateUserButton.addEventListener("click", () => {
     const selectedLayout = siteLayouts.find(
       (layout) => layout.name === layoutSelector.value
