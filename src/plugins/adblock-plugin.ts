41ba7e60e7b1300f09ab9c9c959e017e7b40d8de
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-08-11 09:53:55 -0300

    refactor: Improve AdBlockerPlugin by adding support for blocking and unblocking specific ad selectors

--- src/plugins/adblock-plugin.ts
+++ src/plugins/adblock-plugin.ts
@@ -1,18 +1,16 @@
-import { Plugin } from "./plugin-interface";
-
-export class AdBlockerPlugin implements Plugin {
+export class AdBlockerPlugin {
   private enabled = false;
 
   enable(): void {
     this.enabled = true;
-    console.log("AdBlocker enabled");
     this.blockAds();
   }
 
   disable(): void {
-    console.log("AdBlocker disabled");
     this.enabled = false;
+    this.unblockAds();
   }
+
   private blockAds(): void {
     if (!this.enabled) return;
 
@@ -20,12 +18,39 @@ export class AdBlockerPlugin implements Plugin {
       'div[id^="ad-"]',
       'div[class^="ad-"]',
       'iframe[src*="advertisement"]',
+      'div[role="document"] .ant-modal-content .ant-modal-body .hPZOlHm6KewFnQBA6C_0',
+      'div[role="document"] .ant-modal-root',
+      'div.ant-modal-root',
+      'div.cms-mango-popup[data-type="NotiftPopup"]',
       // Add more ad selectors as needed
     ];
 
     adSelectors.forEach((selector) => {
       const ads = document.querySelectorAll(selector);
-      ads.forEach((ad) => ad.remove());
+      console.log(ads);
+      ads.forEach((ad) => {
+        (ad as HTMLElement).style.display = "none";
+      });
+    });
+  }
+
+  private unblockAds(): void {
+    const adSelectors = [
+      'div[id^="ad-"]',
+      'div[class^="ad-"]',
+      'iframe[src*="advertisement"]',
+      'div[role="document"] .ant-modal-content .ant-modal-body .hPZOlHm6KewFnQBA6C_0',
+      'div[role="document"] .ant-modal-root',
+      'div.cms-mango-popup[data-type="NotiftPopup"]',
+      // Add more ad selectors as needed
+    ];
+
+    adSelectors.forEach((selector) => {
+      const ads = document.querySelectorAll(selector);
+      console.log(ads);
+      ads.forEach((ad) => {
+        (ad as HTMLElement).style.display = "";
+      });
     });
   }
 }
