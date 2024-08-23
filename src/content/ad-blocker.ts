50022c1fce8ba1e5dd7ec04dc2a73033a62f621e
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-08-22 22:24:30 -0300

    refactor: Adjust content.ts to use SRP principle and functional programming

---
+++ src/content/ad-blocker.ts
@@ -0,0 +1,29 @@
+import { AdBlockerPlugin } from '../plugins/adblock-plugin';
+import { createLogger } from '../utils/logger';
+
+const logger = createLogger();
+const adBlocker = AdBlockerPlugin();
+
+export const toggleAdBlocker = (enable: boolean): void => {
+	if (enable) {
+		adBlocker.enable();
+	} else {
+		adBlocker.disable();
+	}
+	localStorage.setItem('adBlockerEnabled', enable.toString());
+	logger.info(`AdBlocker ${enable ? 'enabled' : 'disabled'}`);
+};
+
+export const checkAdBlockerStatusOnLoad = (): void => {
+	const adBlockerEnabled = localStorage.getItem('adBlockerEnabled') === 'true';
+	if (adBlockerEnabled) {
+		adBlocker.enable();
+	} else {
+		adBlocker.disable();
+	}
+	logger.info(`AdBlocker status on load: ${adBlockerEnabled ? 'enabled' : 'disabled'}`);
+};
+
+export const isAdBlockerEnabled = (): boolean => {
+	return true; // or false, depending on the desired logic
+};
---
+++ src/content/message-listener.ts
@@ -0,0 +1,29 @@
+import { handleDepositButton } from '../handlers/deposit-button-handler';
+import { fillForm } from './form-filler';
+import { toggleAdBlocker, isAdBlockerEnabled } from './ad-blocker';
+import { handleButtonWithSpan } from '../handlers/span-button-handler';
+import { SiteLayoutInteface } from '../interfaces/site-layout-interface';
+
+export const initMessageListener = () => {
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    switch (message.action) {
+      case 'handleDepositButton':
+        handleDepositButton();
+        break;
+      case 'fillForm':
+        fillForm(message.layout, message.options);
+        break;
+      case 'toggleAdBlocker':
+        toggleAdBlocker(message.enable);
+        break;
+      case 'checkAdBlockerStatus':
+        sendResponse({ enabled: isAdBlockerEnabled() });
+        break;
+      case 'clickButton':
+        handleButtonWithSpan(message.spanText);
+        sendResponse({ status: 'Button clicked' });
+        break;
+    }
+    sendResponse({ success: true });
+  });
+};
